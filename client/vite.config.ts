832b12b0f2516245e22c0d2ab08a585b60ab1d09
udutvic <udutvic@gmail.com>
2025-04-13 18:30:28 +0200

    commit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 5173
   },
   resolve: {
     alias: {
       '@': '/src',
     },
+  },
+  build: {
+    // Генерувати файл _redirects для Netlify/Render
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+    outDir: 'dist',
   },
 });
